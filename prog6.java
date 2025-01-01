c4be89c9ca4c610929f5552f3c714506f871e2f1
vishal <134676562+vishalalgud@users.noreply.github.com>
2025-01-01 20:27:43 +0530

    Update prog6.java

--- prog6.java
+++ prog6.java
@@ -1,56 +1,56 @@
-class Square
-{
- int a, A;
-public void  inputvalues (int x)
-{
-a=x;
-}
-public void displayArea ()
-{
-A=a*a;
-System.out.println ('Area of square : " +A);
-}
-}
-class Triangle
-{
-double a, b, A;
-public void inputvalues (int x, int y)
-{
-a=x;
-b=y;
-public void displayArea ()
-{
-A=0.5*a*b;
-System.out. println("Area of Triangle: "+A);
-}
-}
-Class Circle
-{
-Double r,A;
-Public void inputvalues(int x)
-{
-r=x;
-}
-Public void displayArea()
-{
-A=3.14*r*r;
-System.out.println(“Area of circle:”+A);
-}
+
+class Square {
+    int a, A;
+
+    public void inputvalues(int x) {
+        a = x;
+    }
+
+    public void displayArea() {
+        A = a * a;
+        System.out.println("Area of square: " + A);
+    }
 }
 
-Public class prog6
-{
-Public static void main(String args[])
-{
-Square S=new Square();
-Triangle T=new Triangle();
-Circle C=new Circle();
-S.inputvalues(10);
-T.inputvalues(9,10);
-C.inputvalues(12);
+class Triangle {
+    double a, b, A;
 
-S.displayArea();
-T.displayArea();
-C.displayArea();
+    public void inputvalues(int x, int y) {
+        a = x;
+        b = y;
+    }
+
+    public void displayArea() {
+        A = 0.5 * a * b;
+        System.out.println("Area of Triangle: " + A);
+    }
 }
+
+class Circle {
+    double r, A;
+
+    public void inputvalues(int x) {
+        r = x;
+    }
+
+    public void displayArea() {
+        A = Math.PI * r * r;
+        System.out.println("Area of Circle: " + A);
+    }
+}
+
+public class prog6 {
+    public static void main(String args[]) {
+        Square S = new Square();
+        Triangle T = new Triangle();
+        Circle C = new Circle();
+
+        S.inputvalues(10);
+        T.inputvalues(9, 10);
+        C.inputvalues(12);
+
+        S.displayArea();
+        T.displayArea();
+        C.displayArea();
+    }
 }
