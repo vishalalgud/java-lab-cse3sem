bf627790cf5ed0a8a2a658990929def3de52b4df
vishal <134676562+vishalalgud@users.noreply.github.com>
2025-01-01 20:29:11 +0530

    Update prog8.java

--- prog8.java
+++ prog8.java
@@ -1,50 +1,47 @@
-class MyThread implements Runnable{
+class MyThread implements Runnable {
     Thread thrd;
-MyThread(String name)
-    {
-	thrd=new Thread(this,name);
-	thrd.start();
+
+    MyThread(String name) {
+        thrd = new Thread(this, name);
+        thrd.start();
     }
-    public void run()
-    {
-	System.out.println(thrd.getName() +"Starting.");
-	 try {
-		for(int count=0;count<10;count++)
-		 {
-			Thread.sleep(400);
-			System.out.println("In " + thrd.getName() + "  count is" + count );
-		 }
-	 }
-	catch(InterruptedExceptionexc)
-	 {
-		System.out.println(thrd.getName()+" interrupted");
-		
-	 }
-	System.out.println(thrd.getName() + " terminating");
+
+    public void run() {
+        System.out.println(thrd.getName() + " Starting.");
+        try {
+            for (int count = 0; count < 10; count++) {
+                Thread.sleep(400);
+                System.out.println("In " + thrd.getName() + " count is " + count);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(thrd.getName() + " interrupted");
+        }
+        System.out.println(thrd.getName() + " terminating");
     }
 }
 
-public class prog8{
+public class prog8 {
 
     public static void main(String[] args) {
-	System.out.println("Main thread starting");
-	MyThread mt1=new MyThread("Child #1");
-	MyThread mt2=new MyThread("Child #2");
-	MyThread mt3=new MyThread("Child #3");
-	 try
-	 {
-	mt1.thrd.join();
-	System.out.println("Child #1 joined");
-	mt2.thrd.join();
-	System.out.println("Child #2 joined");
-	mt3.thrd.join();
-	System.out.println("Child #3 joined");
-	 }
-	
-	catch(InterruptedExceptionexc)
-	 {
-		System.out.println("Main thread interrupted");
-	 }
-	System.out.println("Main thread ending");
+        System.out.println("Main thread starting");
+        
+        // Creating instances of MyThread
+        MyThread mt1 = new MyThread("Child #1");
+        MyThread mt2 = new MyThread("Child #2");
+        MyThread mt3 = new MyThread("Child #3");
+        
+        try {
+            // Main thread waits for all child threads to complete
+            mt1.thrd.join();
+            System.out.println("Child #1 joined");
+            mt2.thrd.join();
+            System.out.println("Child #2 joined");
+            mt3.thrd.join();
+            System.out.println("Child #3 joined");
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted");
+        }
+
+        System.out.println("Main thread ending");
     }
 }
