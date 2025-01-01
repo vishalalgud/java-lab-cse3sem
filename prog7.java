6dbe0773db31a4cdf62e691159de78fdd1974aea
vishal <134676562+vishalalgud@users.noreply.github.com>
2025-01-01 20:28:17 +0530

    Update prog7.java

--- prog7.java
+++ prog7.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
-class Account {
-    static intcurBalance = 1000; // Initial balance
-    int amt;
 
-    public Account() {
-        Scanner s = new Scanner(System.in);
-System.out.println("\nEnter the amount:");
-amt = s.nextInt();
-    }
+class Account {
+    static int curBalance = 1000; // Initial balance
+
+    // Constructor no longer prompts for an amount
+    public Account() {}
+
     void deposit(int amount) {
-curBalance += amount;
-System.out.println("Deposit successful.");
+        curBalance += amount;
+        System.out.println("Deposit successful.");
     }
+
     void withdraw(int amount) {
         try {
             if ((curBalance - amount) < 500)
@@ -22,8 +21,9 @@ System.out.println("Withdrawal successful.");
 System.out.println(le);
         }
     }
+
     void checkBal() {
-System.out.println("\nBalance in your a/c: " + curBalance);
+        System.out.println("\nBalance in your a/c: " + curBalance);
     }
 }
 
@@ -33,38 +33,41 @@ class LessBalanceException extends Exception {
     public LessBalanceException(int x) {
         am = x;
     }
+
     public String toString() {
         return ("You cannot withdraw the amount, less balance " + am + " now.");
     }
 }
+
 public class prog7 {
     public static void main(String[] args) {
-intch;
+        int ch;
         Scanner s = new Scanner(System.in);
-        Account a = new Account();
+        Account a = new Account(); // Constructor no longer needs to accept a Scanner
 
         while (true) {
-System.out.println("1: Deposit\t2: Withdraw\t3: Check Balance\t4: Exit");
-System.out.println("Enter your choice:");
-ch = s.nextInt();
+            System.out.println("1: Deposit\t2: Withdraw\t3: Check Balance\t4: Exit");
+            System.out.println("Enter your choice:");
+            ch = s.nextInt();
             switch (ch) {
                 case 1:
-System.out.println("Enter deposit amount:");
-intdepositAmount = s.nextInt();
-a.deposit(depositAmount);
+                    System.out.println("Enter deposit amount:");
+                    int depositAmount = s.nextInt();
+                    a.deposit(depositAmount);
                     break;
                 case 2:
-System.out.println("Enter withdrawal amount:");
-intwithdrawAmount = s.nextInt();
-a.withdraw(withdrawAmount);
+                    System.out.println("Enter withdrawal amount:");
+                    int withdrawAmount = s.nextInt();
+                    a.withdraw(withdrawAmount);
                     break;
                 case 3:
-a.checkBal();
+                    a.checkBal();
                     break;
                 case 4:
+                    s.close();  // Close the scanner to free resources
                     return;
                 default:
-System.out.println("Invalid choice");
+                    System.out.println("Invalid choice");
                     break;
             }
         }
